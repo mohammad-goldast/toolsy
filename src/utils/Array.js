dafc7a5802d126235f3ef972d9df49a6a7960bd8
Mohammad Goldast <goldast.xps@gmail.com>
2020-11-24 21:14:59 +0330

    Add isEmpty utility for array

---
+++ src/utils/Array.js
@@ -0,0 +1,40 @@
+export const array = {
+    /**
+     *
+     * @param {*} array
+     * Pass your array to isEmpty method
+     * and check it is empty.
+     *
+     * Empty array: []
+     *
+     */
+    isEmpty: (array = []) => {
+        return Array.isArray(array) && array.length === 0;
+    },
+
+    /**
+     *
+     * @param {*} array
+     * Pass your desired array to sort by alphabet for strings and
+     * sort by number for numbers.
+     *
+     * sort([2, 3, 1]);
+     * => [ 1, 2, 3]
+     */
+    sort: (arr = []) => {
+        return arr.sort((a, b) => a - b);
+    },
+
+    /**
+     *
+     * @param {*} array
+     * Pass your desired array and parameter
+     * so that the array is arranged according to your wishes.
+     *
+     * sortByProp([{ number: 2 }, { number: 5 }, { number: 3 }], 'number');
+     * => [ { number: 2 }, { number: 3 }, { number: 5 } ]
+     */
+    sortByProp: (arr = [], prop = '') => {
+        return arr.sort((a, b) => a[prop] - b[prop]);
+    },
+};
